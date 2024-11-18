c80b6cb25db55b786b1ebfc84ecebaf1f9b5639a
myeduarda <myrellae799@gmail.com>
2024-11-18 11:01:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro I?',
+    'Dom Pedro I foi o primeiro imperador do Brasil e proclamou a independência do país em 1822.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'A Revolução Francesa foi um movimento social e político que ocorreu no final do século XVIII e marcou o fim do absolutismo na França.'
+)
+
+criaCartao(
+    'História',
+    'Quem foram os faraós?',
+    'Os faraós eram os governantes do Egito antigo, considerados divindades vivas.'
+)
+
+criaCartao(
+    'História',
+    'Quando ocorreu a Segunda Guerra Mundial?',
+    'A Segunda Guerra Mundial ocorreu entre 1939 e 1945.'
+)
